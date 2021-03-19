57ee5a3e9265df60e56bc8d4e86d00570f0139ab
johnny <jal.johnny13@mail.com>
2021-03-19 20:24:37 -0300

    parte3

--- blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/Model/Postagem.java
+++ blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/Model/Postagem.java
@@ -6,16 +6,19 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @Entity
 @Table(name = "Postagem")
 
 
-public class Postagem {
+		public class Postagem {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long Id;
@@ -27,12 +30,11 @@ public class Postagem {
 	private String texto;
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date Data=new java.sql.Date(System.currentTimeMillis());
-	public long getId() {
-	return Id;
-    	
-    	
-    	
-    }
+	
+		@ManyToOne
+		@JsonIgnoreProperties("Postagem")
+		private Tema tema;
+	
 		public String getTitulo() {
 			return titulo;
 		}
@@ -53,5 +55,15 @@ public class Postagem {
 		}
 		public void setId(long id) {
 			Id = id;
+		}
+		public Tema getTema() {
+		return tema;
+		}
+		public void setTema(Tema tema) {
+		this.tema = tema;
+		}
+		public long getId() {
+			return Id;
+    	
 		}
 }
---
+++ blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/Repository/TemaRepository.java
@@ -0,0 +1,13 @@
+package org.generation.blogPessoal.Repository;
+
+import java.util.List;
+
+import org.generation.blogPessoal.Model.Tema;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TemaRepository extends JpaRepository<Tema, Long>{
+	
+	public List<Tema> findALLByDescricaoContainingIgnoreCase(String descricao);
+	
+
+}
